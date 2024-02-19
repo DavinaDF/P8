34e6259540d952e239f164254091f12e2502e170
Davina Crahet <davina.crahet@gmail.com>
2024-02-19 15:02:31 +0100

    Création accordéon à propos

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,9 +1,24 @@
+import Accordion from "../components/Accordion";
 import BannerAbout from "../components/BannerAbout";
+// import { aboutDescription } from "../data/kasaAbout";
 
 const About = () => {
   return (
     <div className="about">
       <BannerAbout />
+      <Accordion />
+      {/* <div className="about_section">
+        <ul>
+          {aboutDescription.map((description) => (
+            <div className="accordion">
+              <button className="title">{description.title}</button>
+              <div className="content">
+                <p>{description.text}</p>
+              </div>
+            </div>
+          ))}
+        </ul>
+      </div> */}
     </div>
   );
 };
