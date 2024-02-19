5cddea5941f44334b6163e3c34305ab5c36b7244
Davina Crahet <davina.crahet@gmail.com>
2024-02-19 13:59:23 +0100

    é

---
+++ src/components/Accordion.js
@@ -0,0 +1,25 @@
+const Accordion = () => {
+  return (
+    <div className="accordion">
+      <button className="title">Titre de la section</button>
+      <div className="content">
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat
+          minus explicabo itaque? Similique voluptatum pariatur a ea omnis! Et
+          nemo officiis, nisi minima eum cum quisquam officia porro consequatur
+          cupiditate sequi sit fuga tempora dignissimos quod perferendis
+          temporibus delectus deleniti quam deserunt aliquid nobis! Asperiores
+          neque magni consequuntur quos necessitatibus sunt, doloremque dolores
+          eos numquam laboriosam explicabo odio itaque eius reprehenderit illum
+          nisi similique iusto nostrum veritatis quasi dolore autem consequatur
+          error eaque? Corporis voluptatem fugit ratione minus aperiam ipsam
+          similique odit consequuntur at molestiae optio nisi, fugiat autem
+          cupiditate nesciunt ipsum esse ullam quam! Eius corrupti assumenda
+          earum hic?
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Accordion;
--- src/components/LocationCard.js
+++
@@ -1,9 +0,0 @@
-const LocationCard = () => {
-  return (
-    <div className="gallery__element">
-      <h2>Titre de la location</h2>
-    </div>
-  );
-};
-
-export default LocationCard;
