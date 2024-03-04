536a2bd24cbc49f6bb0dc8a30fd4d52a796ecbf9
Davina Crahet <davina.crahet@gmail.com>
2024-03-04 22:44:03 +0100

    Responsive global ok

--- src/components/Review.js
+++ src/components/Review.js
@@ -4,19 +4,17 @@ import starFull from "../assets/images/starFull.svg";
 
 const Review = ({ rating }) => {
   // Variables
-  console.log(rating);
   let countGreyStar = 0;
   const countFullStar = parseInt(rating);
   let greyStar = [];
   let fullStar = [];
-  console.log(countFullStar);
 
   // Calculs
   if (countFullStar < 5) {
     countGreyStar = 5 - countFullStar;
-    console.log(countGreyStar);
   }
 
+  // Création des tableaux
   for (let index = 0; index < countFullStar; index++) {
     fullStar.push(
       <img className="star" key={index} src={starFull} alt="Etoile rouge" />
@@ -28,6 +26,7 @@ const Review = ({ rating }) => {
     );
   }
 
+  // rendu
   return (
     <div className="review">
       {fullStar}
